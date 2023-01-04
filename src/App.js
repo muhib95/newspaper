eea1d66f2ebf2b9ce929bc62f2957bf64030b9ca
Muhibbul hasan <muhibbulhasan6@gmail.com>
2023-01-05 00:20:11 +0600

    data get

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@
 import "./App.css";
 
 function App() {
+  fetch("data.json")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="max-w-screen-2xl mx-auto">
+      <h1>News</h1>
     </div>
   );
 }
